e07875e5c57800ff932f1eb57b1f8377d437f5f7
Sylvia Lin <SylviaLin@via.com.tw>
2021-12-01 17:43:08 +0800

    test37

--- pixetto.cpp
+++ pixetto.cpp
@@ -526,7 +526,7 @@ namespace pixetto {
 			else
 				return 1;
 
-			return data_len;
+			return data_buf[0];
 
 			if (read_len != (data_len-3)) return 2;
 			if (data_buf[data_len-1] != PXT_PACKET_END) return 3;
