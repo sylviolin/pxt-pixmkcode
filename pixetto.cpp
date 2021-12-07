28b6618eaba214afe06fa5d12d8dafacdf7961d9
Sylvia Lin <SylviaLin@via.com.tw>
2021-12-07 10:27:38 +0800

    test4

--- pixetto.cpp
+++ pixetto.cpp
@@ -581,8 +581,8 @@ namespace pixetto {
 				return 1;
 
 			if (read_len != (data_len-3)) return 2;
-			if (data_buf[data_len-1] != PXT_PACKET_END) return 3;
-			if (!verifyChecksum(data_buf, data_len)) return 4;
+			if (data_buf[data_len-1] != PXT_PACKET_END) return data_buf[3];
+			if (!verifyChecksum(data_buf, data_len)) return 9;
 			if (data_buf[2] == 0) return 5; // null packet
 			
 			if (data_buf[2] == PXT_RET_OBJNUM)
